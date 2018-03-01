fc48a7545bc3a1d54f87c4c3756e5621ea2f0007
AlexanderPuentes <alexander.puen91@gmail.com>
2018-02-28 19:59:23 -0500

    migration practice

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301000956) do
+ActiveRecord::Schema.define(version: 20180301005706) do
 
   create_table "videos", force: :cascade do |t|
     t.string "wistio"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
